fbf34094bf9256abcd5a9409d2498da21c54b5f2
Edoardo Operti <s233073@studenti.polito.it>
2017-04-01 22:28:11 +0200

    Modifiche per la Toolbar

--- app/src/main/java/it/polito/mad/mad_app/MainActivity.java
+++ app/src/main/java/it/polito/mad/mad_app/MainActivity.java
@@ -1,22 +1,22 @@
 package it.polito.mad.mad_app;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     class Group {
+
         private String name;
         private ArrayList<String> expenses;
 
@@ -38,9 +38,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("SplitEasy");
+        getSupportActionBar().setLogo(R.drawable.ic_monetization_on_black_24dp);
+
         lv = (ListView) findViewById(R.id.lv);
+
         Group di = new Group("Coinquilini");
         data.add(di);
         di = new Group("Regalo Laurea");
@@ -49,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         data.add(di);
 
         BaseAdapter a=new BaseAdapter() {
+
             @Override
             public int getCount() {
                 return data.size();
@@ -66,10 +75,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
+
                 if (convertView==null){
                     convertView=getLayoutInflater().inflate(R.layout.data_item,parent,false);
-
                 }
+
                 TextView name=(TextView)convertView.findViewById(R.id.name_tv);
                 Group di=data.get(position);
                 name.setText(di.getName());
@@ -82,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 Intent intent=new Intent(
                         getApplicationContext(),GroupActivity.class
                 );
@@ -95,5 +106,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
+
 }
