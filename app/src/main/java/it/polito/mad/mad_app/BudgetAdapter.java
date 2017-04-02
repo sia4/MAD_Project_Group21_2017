4a2fad3840eccf033e0f6ea0eee7406b50859054
Silvia <silviavitali2@gmail.com>
2017-04-02 15:14:40 +0200

    initial commit on budget

---
+++ app/src/main/java/it/polito/mad/mad_app/BudgetAdapter.java
@@ -0,0 +1,49 @@
+package it.polito.mad.mad_app;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class BudgetAdapter extends RecyclerView.Adapter<BudgetAdapter.MyViewHolder> {
+
+    private List<BudgetFragment.cred_deb> budgetData;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name_cred_deb, value_cred_deb;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name_cred_deb = (TextView) view.findViewById(R.id.name_cred_deb);
+            value_cred_deb = (TextView) view.findViewById(R.id.value_cred_deb);
+        }
+    }
+
+
+    public BudgetAdapter(List<BudgetFragment.cred_deb> budgetData) {
+        this.budgetData = budgetData;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.ex_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        BudgetFragment.cred_deb budget = budgetData.get(position);
+        holder.name_cred_deb.setText(budget.getName());
+        holder.value_cred_deb.setText(budget.getValue());
+    }
+
+    @Override
+    public int getItemCount() {
+        return budgetData.size();
+    }
+}
