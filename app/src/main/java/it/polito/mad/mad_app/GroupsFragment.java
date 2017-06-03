dcf4341a026866901006f7922483ad91cd944a36
Luca <s241968@studenti.polito.it>
2017-06-03 10:39:25 +0200

    Luca 03/06
    - altre modifiche

--- app/src/main/java/it/polito/mad/mad_app/GroupsFragment.java
+++ app/src/main/java/it/polito/mad/mad_app/GroupsFragment.java
@@ -12,11 +12,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
