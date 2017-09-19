d0cc52c6c9726edbeefacffe61328e14b767b87d
Luca <s241968@studenti.polito.it>
2017-09-19 17:51:10 +0200

    Luca 19/09
    - AGGIUNTE 28 VALUTE

--- app/src/main/java/it/polito/mad/mad_app/model/Currencies.java
+++ app/src/main/java/it/polito/mad/mad_app/model/Currencies.java
@@ -23,10 +23,69 @@ public class Currencies {
         this.Currencies.put("GBP", "GBP £");
         this.Currencies.put("JPY", "JPY ¥");
 
+        this.Currencies.put("AUD", "AUD $");
+        this.Currencies.put("BGN", "BGN лв");
+        this.Currencies.put("BRL", "BRL R$");
+        this.Currencies.put("CAD", "CAD $");
+        this.Currencies.put("CHF", "CHF CHF");
+        this.Currencies.put("CNY", "CNY ¥");
+        this.Currencies.put("CZK", "CZK Kč");
+        this.Currencies.put("DKK", "DKK kr");
+        this.Currencies.put("HKD", "HKD $");
+        this.Currencies.put("HRK", "HRK kn");
+        this.Currencies.put("HUF", "HUF Ft");
+        this.Currencies.put("IDR", "IDR Rp");
+        this.Currencies.put("ILS", "ILS ₪");
+        this.Currencies.put("INR", "INR ₹");
+        this.Currencies.put("KRW", "KRW ₩");
+        this.Currencies.put("MXN", "MXN $");
+        this.Currencies.put("MYR", "MYR RM");
+        this.Currencies.put("NOK", "NOK kr");
+        this.Currencies.put("NZD", "NZD $");
+        this.Currencies.put("PHP", "PHP ₱");
+        this.Currencies.put("PLN", "PLN zł");
+        this.Currencies.put("RON", "RON lei");
+        this.Currencies.put("RUB", "RUB \u20BD");
+        this.Currencies.put("SEK", "SEK kr");
+        this.Currencies.put("SGD", "SGD $");
+        this.Currencies.put("THB", "THB ฿");
+        this.Currencies.put("TRY", "TRY ₺");
+        this.Currencies.put("ZAR", "ZAR R");
+
         this.Symbols.put("EUR", "€");
         this.Symbols.put("USD", "$");
         this.Symbols.put("GBP", "£");
         this.Symbols.put("JPY", "¥");
+        this.Symbols.put("AUD", "A");
+
+        this.Symbols.put("AUD", "$");
+        this.Symbols.put("BGN", "лв");
+        this.Symbols.put("BRL", "R$");
+        this.Symbols.put("CAD", "$");
+        this.Symbols.put("CHF", "CHF");
+        this.Symbols.put("CNY", "¥");
+        this.Symbols.put("CZK", "Kč");
+        this.Symbols.put("DKK", "kr");
+        this.Symbols.put("HKD", "$");
+        this.Symbols.put("HRK", "kn");
+        this.Symbols.put("HUF", "Ft");
+        this.Symbols.put("IDR", "Rp");
+        this.Symbols.put("ILS", "₪");
+        this.Symbols.put("INR", "₹");
+        this.Symbols.put("KRW", "₩");
+        this.Symbols.put("MXN", "$");
+        this.Symbols.put("MYR", "RM");
+        this.Symbols.put("NOK", "kr");
+        this.Symbols.put("NZD", "$");
+        this.Symbols.put("PHP", "₱");
+        this.Symbols.put("PLN", "zł");
+        this.Symbols.put("RON", "lei");
+        this.Symbols.put("RUB", "\u20BD");
+        this.Symbols.put("SEK", "kr");
+        this.Symbols.put("SGD", "$");
+        this.Symbols.put("THB", "฿");
+        this.Symbols.put("TRY", "₺");
+        this.Symbols.put("ZAR", "R");
 
     }
 
